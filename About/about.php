dc5e2afd8a8e6a13e7340149cfefd9310482bf89
Kawya Awarjana <kawya.evotech@gmail.com>
2024-07-30 04:43:56 +0530

    about complete

--- About/about.php
+++ About/about.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <title>PHYSiO UNLEASHED</title>
     <link rel="icon" href="../resources/img/title_logo.png" />
+    <link rel="stylesheet" href="about.css" />
     <link rel="stylesheet" href="../Header/header.css" />
     <link rel="stylesheet" href="../Footer/footer.css" />
 </head>
@@ -16,11 +17,208 @@
     <?php include "../Header/header.php"; ?>
     <!-- Header -->
 
+    <div class="about--sec1--div1">
+        <div class="about--sec1--div2">
+            <span class="about--sec1--header1">About Us</span>
+        </div>
+    </div>
+
+
+    <!-- ------------section1 start------------ -->
+
+    <section class="about--sec1">
+
+        <div class="about--sec1--div3">
+            <div class="about--sec1--div4">
+                <div class="about--sec1--div5">
+                    <div class="about--sec1--div6">
+                        <span class="about--sec1--header2">About</span>
+                        &nbsp;
+                        <span class="about--sec1--header3">Us</span>
+                    </div>
+                    <div class="about--sec1--div7">
+                        <span class="about--sec1--para1">Physio unleashed Ltd is a home visiting physiotherapy private limited company based in Wirral, Merseyside, UK. We do our operations across the Wirral and Liverpool area currently. All our Physiotherapists are registered with the HCPC and the CSP. They follow on-going professional development, and pride themselves in their quality of treatment, and knowledge base. Our patients are treated holistically, bringing them back from injury, but also aiming to help prevent further injury. We use specific techniques to bring their quality of exercise to a level above where they were pre-injury. Enhancing patient’s quality of life at leisure, in the office/manual workplace or on the sports field.
+                            <br>
+                            We are a cost effective physiotherapy service which provide the service with most reasonable prices and proven highest standard of treatments.
+                        </span>
+                    </div>
+                </div>
+                <div class="about--sec1--div8">
+                    <div class="about--sec1--div9">
+                        <span class="about--sec1--header4">Our</span>
+                        &nbsp;
+                        <span class="about--sec1--header5">Vison</span>
+                    </div>
+                    <div class="about--sec1--div10">
+                        <span class="about--sec1--para2">
+                            To be the leader in sports and musculoskeletal Physiotherapy home service provider, while striving to be the first choice among clients with 100% satisfaction and appreciation.
+                        </span>
+                    </div>
+                </div>
+                <div class="about--sec1--div11">
+                    <div class="about--sec1--div12">
+                        <span class="about--sec1--header6">Our</span>
+                        &nbsp;
+                        <span class="about--sec1--header7">Mission</span>
+                    </div>
+                    <div class="about--sec1--div13">
+                        <span class="about--sec1--para3">
+                            We unleashed our maximum potential to improve your wellbeing and speedy recovery in your homely environment with high standard of skills, morals and professionalism. We provide not just physiotherapy but a deal with your inner soul with extreme humanism and empathy. Our net profit is your blessings and happiness.
+                        </span>
+                    </div>
+                    <div class="about--sec1--div14">
+                        <span class="about--sec1--header8">
+                            Our Promises
+                        </span>
+                    </div>
+                    <div class="about--sec1--div15">
+                        <span class="about--sec1--para4">
+                            <span><b>• We keep you active – </b>So that you can always go to work, sit and stand if you want, engage in sports, take your kids out during the weekends, perform your moral obligations without pain having to come in between. </span>
+                            <br>
+                            <span><b>• We help you Stay free from painkillers – </b>So that you are not always running to the pharmacy to buy pain relief drugs or reaching in the drawer for pain relief medication these pain Relief Medications are not Good for your health and the only relieve your pain for a while and pain comes back in few hours and cause long term side effects that may be too expensive to cure or worst off all incurable. </span>
+                            <br>
+                            <span><b>• We help you avoid dangerous surgery – </b>Treating the root cause of your pain is one way to help solve the underlying problems you are facing there no waiting to see any surgeon who might want to use complex surgeries to treat your pain. </span>
+                            <br>
+                            <span><b>• Find out what’s wrong – </b>We will do an extensive review and evaluation, giving a clear explanation and of what is wrong which will give you peace of mind and ensure that your problem is solved and quickly refer you to another specialist when it’s out of scope of our practice and we will provide you with some home exercises that will speed up your recovery. </span>
+                            <br>
+                            <span><b>• Get more enjoyment out of life and improve the quality of life – </b>Booking an appointment with us is quarter to enjoying quality time with family and friends that aches and pains can steal from you, and finally get a good night sleep without tossing and turning trying to find that one position that feels right. </span>
+                        </span>
+                    </div>
+                    <div class="about--sec1--div16">
+                        <span class="about--sec1--header9">
+                            Our Targets
+                        </span>
+                    </div>
+                    <div class="about--sec1--div17">
+                        <span class="about--sec1--para5">
+                            <span>• Who thought their pain will go away but it didn’t go.</span>
+                            <br>
+                            <span>• Paid several visits to the Doctor, only to be told to “rest” and “take some painkillers” – then weeks later go back to see the Doctor again, only to be prescribed more rest, and an even stronger dose of painkillers.</span>
+                            <br>
+                            <span>• You have been to other physiotherapy clinics and think you didn’t get the best attention deserved by being treated by the clinicians who work under supervision (interns) and then feels it would be a good idea to rest because the pain didn’t get better, but all they did was to make the area worst off.</span>
+                            <br>
+                            <span>• Been told by friends that everybody has back pain or neck pain and its part of life.</span>
+                            <br>
+                            <span>• You have been told that physiotherapy is only in the hospitals and will not help you, instead do a massage at the hair saloon or some local spa.</span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="about--sec1lg">
+
+        <div class="about--sec1--div3lg">
+            <div class="about--sec1--div4lg">
+                <div class="about--sec1--div5lg">
+                    <div class="about--sec1--div6lg">
+                        <span class="about--sec1--header2lg">About</span>
+                        &nbsp;
+                        <span class="about--sec1--header3lg">Us</span>
+                    </div>
+                    <div class="about--sec1--div7lg">
+                        <span class="about--sec1--para1lg">Physio unleashed Ltd is a home visiting physiotherapy private limited company based in Wirral, Merseyside, UK. We do our operations across the Wirral and Liverpool area currently. All our Physiotherapists are registered with the HCPC and the CSP. They follow on-going professional development, and pride themselves in their quality of treatment, and knowledge base. Our patients are treated holistically, bringing them back from injury, but also aiming to help prevent further injury. We use specific techniques to bring their quality of exercise to a level above where they were pre-injury. Enhancing patient’s quality of life at leisure, in the office/manual workplace or on the sports field.
+                            <br>
+                            We are a cost effective physiotherapy service which provide the service with most reasonable prices and proven highest standard of treatments.
+                        </span>
+                    </div>
+                </div>
+                <div class="about--sec1--div8lg">
+                    <div class="about--sec1--div9lg">
+                        <span class="about--sec1--header4lg">Our</span>
+                        &nbsp;
+                        <span class="about--sec1--header5lg">Mission</span>
+                    </div>
+                    <div class="about--sec1--div10lg">
+                        <span class="about--sec1--para2lg">
+                            We unleashed our maximum potential to improve your wellbeing and speedy recovery in your homely environment with high standard of skills, morals and professionalism. We provide not just physiotherapy but a deal with your inner soul with extreme humanism and empathy. Our net profit is your blessings and happiness.
+                        </span>
+                    </div>
+                </div>
+                <div class="about--sec1--div12lg">
+                    <span class="about--sec1--header6lg">Our</span>
+                    &nbsp;
+                    <span class="about--sec1--header7lg">Vison</span>
+                </div>
+                <div class="about--sec1--div13lg">
+                    <span class="about--sec1--para3lg">
+                        To be the leader in sports and musculoskeletal Physiotherapy home service provider, while striving to be the first choice among clients with 100% satisfaction and appreciation.
+                    </span>
+                </div>
+                <div class="about--sec1--div19md">
+                    <img src="../resources/img/about/1.png" class="about--sec1--img1">
+                </div>
+            </div>
+            <div class="about--sec1--div18lg">
+                <div class="about--sec1--div11lg">
+                    <div class="about--sec1--div14lg">
+                        <span class="about--sec1--header8lg">
+                            Our Promises
+                        </span>
+                    </div>
+                    <div class="about--sec1--div15lg">
+                        <span class="about--sec1--para4lg">
+                            <span><b>• We keep you active – </b>So that you can always go to work, sit and stand if you want, engage in sports, take your kids out during the weekends, perform your moral obligations without pain having to come in between. </span>
+                            <br>
+                            <span><b>• We help you Stay free from painkillers – </b>So that you are not always running to the pharmacy to buy pain relief drugs or reaching in the drawer for pain relief medication these pain Relief Medications are not Good for your health and the only relieve your pain for a while and pain comes back in few hours and cause long term side effects that may be too expensive to cure or worst off all incurable. </span>
+                            <br>
+                            <span><b>• We help you avoid dangerous surgery – </b>Treating the root cause of your pain is one way to help solve the underlying problems you are facing there no waiting to see any surgeon who might want to use complex surgeries to treat your pain. </span>
+                            <br>
+                            <span><b>• Find out what’s wrong – </b>We will do an extensive review and evaluation, giving a clear explanation and of what is wrong which will give you peace of mind and ensure that your problem is solved and quickly refer you to another specialist when it’s out of scope of our practice and we will provide you with some home exercises that will speed up your recovery. </span>
+                            <br>
+                            <span><b>• Get more enjoyment out of life and improve the quality of life – </b>Booking an appointment with us is quarter to enjoying quality time with family and friends that aches and pains can steal from you, and finally get a good night sleep without tossing and turning trying to find that one position that feels right. </span>
+                        </span>
+                    </div>
+
+                </div>
+            </div>
+            <div class="about--sec1--div19lg">
+                <img src="../resources/img/about/1.png" class="about--sec1--img1">
+                <img src="../resources/img/about/2.png" class="about--sec1--img2">
+            </div>
+        </div>
+
+    </section>
+
+    <!-- ------------section1 end------------ -->
+
+
+    <!-- ------------section2 start------------ -->
+
+    <section class="about--sec2">
+
+        <div class="about--sec2--div1">
+
+            <button class="accordion">Section 1</button>
+            <div class="panel">
+                <p>Lorem ipsum...</p>
+            </div>
+
+            <button class="accordion">Section 2</button>
+            <div class="panel">
+                <p>Lorem ipsum...</p>
+            </div>
+
+            <button class="accordion">Section 3</button>
+            <div class="panel">
+                <p>Lorem ipsum...</p>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- ------------section2 end------------ -->
 
 
     <!-- Footer -->
     <?php include "../Footer/footer.php"; ?>
     <!-- Footer -->
+
+    <script src="about.js"></script>
+    <script src="../Header/header.js"></script>
 
 </body>
 
