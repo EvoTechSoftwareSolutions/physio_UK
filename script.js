155d62f881b49dcf210d0534081f9d299cbb086c
EvoTechSoftwareSolutions <sandila.evotech@gmail.com>
2024-08-08 11:21:30 +0530

    adad

--- script.js
+++ script.js
@@ -122,16 +122,16 @@ function loadPie(valueArray) {
           label: "Number of appointments",
           data: valueArray,
           backgroundColor: [
-            "rgba(255, 99, 132, 0.4)",
             "rgba(54, 162, 235, 0.4)",
+            "rgba(255, 99, 132, 0.4)",
             "rgba(255, 206, 86, 0.4)",
             "rgba(75, 192, 192, 0.4)",
             "rgba(153, 102, 255, 0.4)",
             "rgba(255, 159, 64, 0.4)",
           ],
           borderColor: [
-            "rgba(255, 99, 132, 1)",
             "rgba(54, 162, 235, 1)",
+            "rgba(255, 99, 132, 1)",
             "rgba(255, 206, 86, 1)",
             "rgba(75, 192, 192, 1)",
             "rgba(153, 102, 255, 1)",
