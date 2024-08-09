5fb7afcd917fddc70aab4be3b8a147a11eb7ea6f
EvoTechSoftwareSolutions <sandila.evotech@gmail.com>
2024-08-09 09:37:19 +0530

    adadad

--- Admin/index.js
+++ Admin/index.js
@@ -1,91 +1,121 @@
 (function ($) {
-    "use strict";
+  function getChartData() {
+    var f = new FormData();
+    f.append("act", "loadChart");
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
+    var r = new XMLHttpRequest();
+    r.onreadystatechange = function () {
+      if (r.readyState == 4) {
+        try {
+          // Attempt to parse the response text as JSON
+          const jsonResponse = JSON.parse(r.responseText);
+
+          if (jsonResponse[0] == "success") {
+            return([jsonResponse[1], jsonResponse[2]]);
+            //loadPie(jsonResponse[3]);
+          } else {
+            Swal.fire({
+              title: "Failed",
+              text: "Something went wrong",
+              icon: "error",
+            });
+          }
+        } catch (e) {
+          Swal.fire({
+            title: "Failed",
+            text: r.responseText,
+            icon: "error",
+          });
+        }
+      }
     };
-    spinner();
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
 
+    r.open("POST", "./Backend/backend.php", true);
+    r.send(f);
+  }
 
-    // Sidebar Toggler
-    $('.sidebar-toggler').click(function () {
-        $('.sidebar, .content').toggleClass("open");
-        return false;
-    });
+  ("use strict");
 
-    
-    // Chart Global Color
-    Chart.defaults.color = "#6C7293";
-    Chart.defaults.borderColor = "#000000";
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
 
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
-    // Salse & Revenue Chart
-    var ctx2 = $("#salse-revenue").get(0).getContext("2d");
-    var myChart2 = new Chart(ctx2, {
-        type: "line",
-        data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-            datasets: [{
-                    label: "Sales",
-                    data: [15, 30, 55, 45, 70, 65, 85],
-                    backgroundColor: "rgba(235, 22, 22, .7)",
-                    fill: true
-                },
-                {
-                    label: "Revenue",
-                    data: [99, 135, 170, 130, 190, 180, 270],
-                    backgroundColor: "rgba(235, 22, 22, .5)",
-                    fill: true
-                }
-            ]
-            },
-        options: {
-            responsive: true
-        }
-    });
+  // Sidebar Toggler
+  $(".sidebar-toggler").click(function () {
+    $(".sidebar, .content").toggleClass("open");
+    return false;
+  });
 
-    // Single Bar Chart
-    var ctx4 = $("#bar-chart").get(0).getContext("2d");
-    var myChart4 = new Chart(ctx4, {
-        type: "bar",
-        data: {
-            labels: ["Italy", "France", "Spain", "USA", "Argentina"],
-            datasets: [{
-                backgroundColor: [
-                    "rgba(235, 22, 22, .7)",
-                    "rgba(235, 22, 22, .6)",
-                    "rgba(235, 22, 22, .5)",
-                    "rgba(235, 22, 22, .4)",
-                    "rgba(235, 22, 22, .3)"
-                ],
-                data: [55, 49, 44, 24, 15]
-            }]
+  // Chart Global Color
+  Chart.defaults.color = "#6C7293";
+  Chart.defaults.borderColor = "#000000";
+
+  // Salse & Revenue Chart
+  var ctx2 = $("#salse-revenue").get(0).getContext("2d");
+  var myChart2 = new Chart(ctx2, {
+    type: "line",
+    data: {
+      labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+      datasets: [
+        {
+          label: "Sales",
+          data: [15, 30, 55, 45, 70, 65, 85],
+          backgroundColor: "rgba(235, 22, 22, .7)",
+          fill: true,
         },
-        options: {
-            responsive: true
-        }
-    });
+        {
+          label: "Revenue",
+          data: [99, 135, 170, 130, 190, 180, 270],
+          backgroundColor: "rgba(235, 22, 22, .5)",
+          fill: true,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+    },
+  });
 
-
-    
+  // Single Bar Chart
+  var ctx4 = $("#bar-chart").get(0).getContext("2d");
+  var myChart4 = new Chart(ctx4, {
+    type: "bar",
+    data: {
+      labels: ["Italy", "France", "Spain", "USA", "Argentina"],
+      datasets: [
+        {
+          backgroundColor: [
+            "rgba(235, 22, 22, .7)",
+            "rgba(235, 22, 22, .6)",
+            "rgba(235, 22, 22, .5)",
+            "rgba(235, 22, 22, .4)",
+            "rgba(235, 22, 22, .3)",
+          ],
+          data: [55, 49, 44, 24, 15],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+    },
+  });
 })(jQuery);
-
